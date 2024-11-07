4bf7f907c285d175012a19e8617e5fec8910f5e1
hungdeptrai022 <quangluong03012004@gmail.com>
2024-11-07 07:26:47 +0700

    fix Home

--- Playeractive_FE/src/App.jsx
+++ Playeractive_FE/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div >
         <Routes>
-          <Route path='/' element ={<Home/>}/>
+          <Route path='*' element ={<Home/>}/>
           <Route path='/login' element ={<Login/>}/>
         </Routes>     
     </div>
